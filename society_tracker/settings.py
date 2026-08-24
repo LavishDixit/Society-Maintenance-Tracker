8f2ee666eaba68e117e0714dc91dae9c682594b0
Lavish Dixit <dixitlavish2005@gmail.com>
2026-08-24 22:25:55 +0530

    Configure Brevo SMTP email notifications

--- society_tracker/settings.py
+++ society_tracker/settings.py
@@ -155,16 +155,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ---------------------------------------------------------------------------
 # Email (SendGrid via SMTP relay)
 # ---------------------------------------------------------------------------
-EMAIL_BACKEND = config(
-    'EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend'
+MAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.console.EmailBackend'
 )
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
+
+EMAIL_HOST = config('EMAIL_HOST', default='smtp-relay.brevo.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='apikey')  # literal string 'apikey' for SendGrid
-EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@societytracker.local')
 
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 # ---------------------------------------------------------------------------
 # App-specific config
 # ---------------------------------------------------------------------------
